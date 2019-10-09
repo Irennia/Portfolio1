9cae47a790b4fe5f23b70cff5c760bfda2e45c8d
Unknown <irean91@gmail.com>
2019-10-09 19:26:57 +0200

    primer commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+$("#contenedor").html('index.html');
+
+function llamarAlert() {
+        alert("Bienvenido a mi Portfolio!")
+    }
+    
+
+$(document).ready(function(){
+    setTimeout(function(){llamarAlert()}, 5000)});
+
