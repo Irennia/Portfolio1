9cd6018af79bfc0e301296633791a00851aeac47
Unknown <irean91@gmail.com>
2019-11-22 14:56:22 +0100

    update presupuesto // add secAdmin.html

---
+++ js/presupuesto.js
@@ -0,0 +1,10 @@
+function presupuestoTotal(){
+    var nombre = document.getElementById("nombre").value
+//    alert(nombre);
+}
+
+//$("#presupuesto").val("#nombre")
+
+$("#presupuesto").on("click", function(){
+ $("#presupuesto").val("#nombre")})
+    
