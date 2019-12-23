4e4e8f849033e4403d96a0fb5b799a3e0853c055
Unknown <irean91@gmail.com>
2019-12-23 09:24:02 +0100

    presupuesto, FALTA SUMAR RESULTADO

--- js/presupuesto.js
+++ js/presupuesto.js
@@ -1,10 +1,102 @@
-function presupuestoTotal(){
-    var nombre = document.getElementById("nombre").value
-//    alert(nombre);
-}
+//function calculo(){
+//    
+//    var presuTotal = 0;
+//    var nombre = document.getElementById("nombre").value
+//    
+//    $("#presupuesto").val("#nombre").show
+//   //alert(nombre);
+//}
 
-//$("#presupuesto").val("#nombre")
+//$(document).ready(function () {
+//    $("#nombre").keyup(function () {
+//        var value = $(this).val();
+//        $("#presupuesto").val(value);
+//    });
+//});
 
-$("#presupuesto").on("click", function(){
- $("#presupuesto").val("#nombre")})
+var totalTipoWeb = 0;
+var descuentoPlazos= 0;
+var total = 0;
+var sumaT = 0;
+
+$(document).ready(function () {  
     
+    $("#tipoWeb").change(function (e) {
+            $('#tipoWeb').each(function(){
+                var precio = $(this).val();
+                var valor = $("#tipoWeb").val();
+                 if (precio == "40")
+                  totalTipoWeb += parseInt(valor);
+                 if (precio == "50")
+                  totalTipoWeb += parseInt(valor);
+                 if (precio == "60")
+                  totalTipoWeb += parseInt(valor);
+
+                $("#web").val(totalTipoWeb);
+            })
+        })
+
+        $("#plazos").change(function (e) {
+                $('#plazos').each(function(){
+                    var descuento = $(this).val();
+                    var valor = $("#web").val();
+                     if (descuento == "0.05")
+                      descuentoPlazos = valor / 1.05;
+                     if (descuento == "0.10")
+                      descuentoPlazos = valor / 1.10;
+                     if (descuento == "0.15")
+                      descuentoPlazos = valor / 1.15;
+                    if (descuento == "0.20")
+                      descuentoPlazos = valor / 1.20;
+            
+                    $("#plazosTotal").val(descuentoPlazos);
+
+                })
+        })
+//        $("#plazos").change(function (e) {
+//                $('#plazos').each(function(){
+//                    var descuento = $(this).val();
+//                    var valor = $("#plazos").val();
+//                     if (descuento == "0.05")
+//                      descuentoPlazos += parseFloat(valor);
+//                     if (descuento == "0.10")
+//                      descuentoPlazos += parseFloat(valor);
+//                     if (descuento == "0.15")
+//                      descuentoPlazos += parseFloat(valor);
+//                    if (descuento == "0.20")
+//                      descuentoPlazos += parseFloat(valor);
+//            
+//                    $("#plazosTotal").val(descuentoPlazos);
+//
+//                })
+//        })
+        
+});
+
+//$(document).ready(function1() {
+//    $(".total").change(function1(e) {
+//        $('.total').each(function1(){
+//            var cantidad = $(this).val();
+//
+//            var presu += cantidad;
+//
+//            $("#prepTotal").val(presu);
+//        })
+//    })
+//});
+
+    function sumar(valor) {
+        total += valor;
+        document.formulario.seccionesTotal.value = total;
+    }
+
+    function restar(valor) { 
+        total -= valor;  
+        document.formulario.seccionesTotal.value = total; 
+    }
+
+    function sumaTotal(v) {
+        sumaT += v;
+        
+        document.formulario.prepTotal.value = sumaT;
+    }
